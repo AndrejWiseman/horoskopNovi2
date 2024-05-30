0a4b8e8a3e4d57247f1e8c7e204686ed756bab79
Andrej <533737@gmail.com>
2024-05-30 17:09:52 +0200

    static

--- build_files.sh
+++ build_files.sh
@@ -13,8 +13,3 @@ python -m pip install -r requirements.txt
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
-
-
-
-#pip install -r requirements.txt
-#python3.9 manage.py collectstatic
